67b7b6da7abe76d83613b892215b7cd9d08fecd3
metalmodo <metalmodo@localhost.localdomain>
2023-09-14 10:14:36 -0700

    moved Xmls to new data folder, worked on card.cs

--- source/card.cs
+++ source/card.cs
@@ -1,21 +1,37 @@
 using System;
 using System.IO;
 using System.Xml;
+using System.Collections.Generic;
 
 class MainLoop {
+  public static Random rng = new Random();
+
   public static int Main() {
 
     XmlDocument doc = new XmlDocument();
-    doc.Load("cards.xml");
-    XmlNodeList cards = doc.GetElementsByTagName("card");
+    doc.Load("data/cards.xml");
+    String dataPath = Console.ReadLine();
+    if (dataPath.Length > 0) {
+      doc.Load(dataPath);
+    }
+    /*XmlNodeList cards = doc.GetElementsByTagName("card");
     foreach (XmlNode cardInfo in cards)
     {
       Card card = new Card(cardInfo);
       Console.WriteLine(card.GetName());
       Console.WriteLine(card.GetLeftAction().InnerXml);
-    }
+    }*/
+    int[] amounts = {2, 2, 1, 5};
+    Deck deck = new Deck(doc, amounts);
+    deck.FisherYatesShuffle();
+    /*Console.WriteLine(deck.DrawOne());
+    Console.WriteLine(deck.DrawOne());
+    Console.WriteLine(deck.DrawOne()); */
     Console.WriteLine("-----------------------");
     //Console.WriteLine(Alignment.Detroit.ToString());
+    Enemy ig = new Enemy();
+    Console.WriteLine(ig.ToString());
+    Console.ReadLine();
     return 0;
   }
 }
@@ -82,18 +98,78 @@ class Card {
   {
     throw new NotImplementedException();
   }
+
+  public override String ToString()
+  {
+    return "[" + this.name + "]";
+  }
 }
 
+class Deck {
+
+  protected List<Card> tableDeck;
+  protected List<Card> hand;
+  protected List<Card> discardPile;
+  protected List<Card> graveyard;
+
+  public Deck(XmlDocument cardsDoc, int[] amounts) {
+    tableDeck = new List<Card>();
+    hand = new List<Card>();
+    XmlNodeList types = cardsDoc.GetElementsByTagName("card");
+    for (int i = 0; i < types.Count; i++) {
+      if (types.Count > amounts.Length)  { throw new NotImplementedException();}
+      for(int j = 0; j < amounts[i]; j++) {
+
+        tableDeck.Add(new Card(types[i]));
+      }
+    }
+  }
+
+  public void FisherYatesShuffle() {
+    int n = tableDeck.Count;
+    while(n > 1) {
+      n--;
+      int k = MainLoop.rng.Next(n + 1);
+      Card swapValue = tableDeck[k];
+      tableDeck[k] = tableDeck[n];
+      tableDeck[n] = swapValue;
+    }
+  }
+
+  public bool DrawOne() {
+    if (tableDeck.Count < 1)  
+      return false;
+    Card ret = tableDeck[0];
+    tableDeck.RemoveAt(0);
+    hand.Add(ret);
+    return true;
+  }
+
+  public void Draw(int amount) {
+  while(amount > 0)
+  {
+    if(DrawOne())
+    {}
+    else
+    {
+      tableDeck = discardPile;
+      discardPile = new List<Card>();
+      FisherYatesShuffle();
+      DrawOne();
+    }
+    amount--; // naturally goes to 0 if no cards actually drawn
+  }
+}
 public enum Alignment {
   Fae,
   Independent,
   Goblin,
   PowerHungry,
   Underground,
-  Detroit,
+  Decept,
   Demon,
   Imp,
-  Nonce,
+  NullExceptions,
   Ghoul
 }
 
@@ -146,16 +222,23 @@ class Enemy : Creature {
   {
 
   }
-  
-  public Enemy(int health, int maxHealth, String name, Alignment align = Alignment.Goblin, int manaLevel=0)
+
+  public Enemy(int health, int maxHealth, String name, Alignment align = Alignment.Goblin, int manaLevel=0):base(health, maxHealth, name, align, manaLevel)
   {
     throw new NotImplementedException();
-    //add cards
+    //add cards to enemy deck
   }
 
   public override String ToString()
   {
-    return "hello, Scum!, my name is " + base.name + "and my health is "
-      + base.health + "out of"+ base.maxHealth;
+    return "hello, Scum!, my name is " + base.name + " and my health is "
+      + base.health + " out of "+ base.maxHealth;
   }
+}
+
+class StateManager {
+  private Creature player = new Creature(100, 100, "<placeholder name>", Alignment.Underground, 0);
+
+
+
 }
